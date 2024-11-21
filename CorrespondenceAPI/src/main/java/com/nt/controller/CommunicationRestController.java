23160dc77fc1f6f2974d5b96b1b99e922b90a5d6
praveenkumar <praveenkumar@localhost>
2024-11-21 13:00:36 +0530

    commit

--- CorrespondenceAPI/src/main/java/com/nt/controller/CommunicationRestController.java
+++ CorrespondenceAPI/src/main/java/com/nt/controller/CommunicationRestController.java
@@ -1,5 +1,8 @@
 package com.nt.controller;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,12 +10,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CommunicationRestController {
-	
+
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<String> handleAllExceptions(Exception exception){
-		
-		return new ResponseEntity<String>(exception.getMessage(),HttpStatus.BAD_REQUEST);
-		
+	public ResponseEntity<String> handleAllExceptions(Exception exception) {
+
+		return new ResponseEntity<String>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+
 	}
 
+	@ExceptionHandler(FileNotFoundException.class)
+	public ResponseEntity<String> handleFIleNotFoundExceptions(FileNotFoundException exception) {
+
+		return new ResponseEntity<String>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+
+	}
+	@ExceptionHandler(IOException.class)
+	public ResponseEntity<String> handleIOExceptions(IOException exception){
+		
+		return new ResponseEntity<String>(exception.getMessage(),HttpStatus.BAD_REQUEST);
+	}
 }
