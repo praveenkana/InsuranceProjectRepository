27205b9c60ea176c7ec8a4fede6e992064d343a3
praveenkumar <praveenkumar@localhost>
2024-11-21 12:57:49 +0530

    commit

--- CorrespondenceAPI/src/main/java/com/nt/controller/CmnController.java
+++ CorrespondenceAPI/src/main/java/com/nt/controller/CmnController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class CmnController {
 	@Autowired
 	private ICorrespondenceService service;
 	
+	@GetMapping("/process-communication")
 	public ResponseEntity<CmnSummary> processCommunication() throws FileNotFoundException, MessagingException, IOException{
 		
 		CmnSummary summary = service.processPendingCommunication();
